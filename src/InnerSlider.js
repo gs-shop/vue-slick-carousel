5a0aea59de6baf952362e9cc33a291176e342e71
KyuWoo Choi <kyuwoo.choi@gmail.com>
2019-11-24 04:03:13 +0900

    feat: implement innerSlider.ssrInit

--- src/InnerSlider.js
+++ src/InnerSlider.js
@@ -1,8 +1,12 @@
 import SliderTrack from '@/SliderTrack'
 import SliderArrow from '@/SliderArrow'
 import SliderDots from '@/SliderDots'
+
 import defaultProps from '@/defaultProps'
 import initialState from '@/initialState'
+
+import { getStyle } from '@/vNodeUtils'
+import { getPreClones, getPostClones } from '@/innerSliderUtils'
 
 export default {
   name: 'InnerSlider',
@@ -26,6 +30,8 @@ export default {
     this.callbackTimers = []
     this.clickable = true
     this.debouncedResize = null
+
+    this.ssrInit()
   },
   methods: {
     slickPrev() {
@@ -42,6 +48,63 @@ export default {
     },
     autoPlay() {
       throw Error('not implemented yet')
+    },
+    ssrInit() {
+      const spec = {
+        ...this.$props,
+        ...this.$data,
+        slideCount: this.slideCount,
+      }
+      const preClones = getPreClones(spec)
+      const postClones = getPostClones(spec)
+      if (this.variableWidth) {
+        let trackWidth = [],
+          trackLeft = []
+        let childrenWidths = []
+        this.$slots.default.forEach(child => {
+          const { width } = getStyle(child)
+          childrenWidths.push(width)
+          trackWidth.push(width)
+        })
+        for (let i = 0; i < preClones; i++) {
+          trackLeft.push(childrenWidths[childrenWidths.length - 1 - i])
+          trackWidth.push(childrenWidths[childrenWidths.length - 1 - i])
+        }
+        for (let i = 0; i < postClones; i++) {
+          trackWidth.push(childrenWidths[i])
+        }
+        for (let i = 0; i < this.currentSlide; i++) {
+          trackLeft.push(childrenWidths[i])
+        }
+        // filter undefined
+        trackWidth = trackWidth.filter(o => o)
+        trackLeft = trackLeft.filter(o => o)
+        let trackStyle = {
+          width: `calc(${trackWidth.join(' + ')})`,
+          left: `calc(${trackLeft.map(o => `-${o}`).join(' + ')})`,
+        }
+        if (this.centerMode) {
+          let currentWidth = childrenWidths[this.currentSlide]
+          trackStyle.left = `calc(${trackLeft
+            .map(o => `-${o}`)
+            .join(' + ')} + (100% - ${currentWidth}) / 2 )`
+        }
+        this.trackStyle = trackStyle
+      } else {
+        let slideCount = preClones + postClones + this.slideCount
+        let trackWidth = (100 / this.slidesToShow) * slideCount
+        let slideWidth = 100 / slideCount
+        let trackLeft =
+          (-slideWidth * (preClones + this.currentSlide) * trackWidth) / 100
+        if (this.centerMode) {
+          trackLeft += (100 - (slideWidth * trackWidth) / 100) / 2
+        }
+        this.slideWidth = slideWidth + '%'
+        this.trackStyle = {
+          width: trackWidth + '%',
+          left: trackLeft + '%',
+        }
+      }
     },
   },
   render() {
--- src/innerSliderUtils.js
+++ src/innerSliderUtils.js
@@ -4,3 +4,20 @@ export const canUseDOM = () =>
     window.document &&
     window.document.createElement
   )
+
+export const getPreClones = spec => {
+  if (spec.unslick || !spec.infinite) {
+    return 0
+  }
+  if (spec.variableWidth) {
+    return spec.slideCount
+  }
+  return spec.slidesToShow + (spec.centerMode ? 1 : 0)
+}
+
+export const getPostClones = spec => {
+  if (spec.unslick || !spec.infinite) {
+    return 0
+  }
+  return spec.slideCount
+}
