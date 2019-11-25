55ad441d0c94aca50b1e47a3989c82284616a62d
KyuWoo Choi <kyuwoo.choi@gmail.com>
2019-11-25 14:10:10 +0900

    feat: implement dots

--- src/SliderDots.js
+++ src/SliderDots.js
@@ -1,9 +1,66 @@
 import { PROP_KEYS } from '@/innerSliderUtils'
+import { mergeVNodeData } from '@/vNodeUtils'
+
+const getDotCount = spec => {
+  let dots
+
+  if (spec.infinite) {
+    dots = Math.ceil(spec.slideCount / spec.slidesToScroll)
+  } else {
+    dots =
+      Math.ceil((spec.slideCount - spec.slidesToShow) / spec.slidesToScroll) + 1
+  }
+
+  return dots
+}
 
 export default {
   name: 'SliderDots',
   props: [...PROP_KEYS.DOT, 'clickHandler'],
-  render() {
-    return <div>dots</div>
+  render(h) {
+    let dotCount = getDotCount({
+      slideCount: this.slideCount,
+      slidesToScroll: this.slidesToScroll,
+      slidesToShow: this.slidesToShow,
+      infinite: this.infinite,
+    })
+
+    // Apply join & split to Array to pre-fill it for IE8
+    //
+    // Credit: http://stackoverflow.com/a/13735425/1849458
+    let dots = Array.apply(
+      null,
+      Array(dotCount + 1)
+        .join('0')
+        .split(''),
+    ).map((x, i) => {
+      let leftBound = i * this.slidesToScroll
+      let rightBound = i * this.slidesToScroll + (this.slidesToScroll - 1)
+      let className = {
+        'slick-active':
+          this.currentSlide >= leftBound && this.currentSlide <= rightBound,
+      }
+
+      let dotOptions = {
+        message: 'dots',
+        index: i,
+        slidesToScroll: this.slidesToScroll,
+        currentSlide: this.currentSlide,
+      }
+
+      let onClick = this.clickHandler.bind(this, dotOptions)
+      return (
+        <li key={i} class={className} onClick={onClick}>
+          {this.customPaging(h, i)}
+        </li>
+      )
+    })
+
+    const wrapper = this.appendDots(h, dots)
+    mergeVNodeData(wrapper, 'class', {
+      [this.dotsClass]: true,
+    })
+    wrapper.children = dots
+    return wrapper
   },
 }
