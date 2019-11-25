43e6bd3758edf50ea673d3408a6b095228f40265
KyuWoo Choi <kyuwoo.choi@gmail.com>
2019-11-25 15:27:39 +0900

    chore: update demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,12 +1,58 @@
 <template>
   <div id="app">
-    <vue-slick-carousel></vue-slick-carousel>
+    <vue-slick-carousel v-bind="settings">
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+      <div>item</div>
+    </vue-slick-carousel>
   </div>
 </template>
 
 <script>
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 export default {
   name: 'app',
+  data() {
+    return {
+      settings: {
+        slidesToShow: 0,
+        lazyLoad: true,
+        touchMove: true,
+        pauseOnHover: true,
+        accessibility: true,
+        centerMode: true,
+        dots: true,
+        pauseOnDotsHover: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 300,
+            settings: 'unslick',
+          },
+        ],
+      },
+    }
+  },
 }
 </script>
 
