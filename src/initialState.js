4f82e7ce1767830f6dc5c3340eb5571e0392b394
KyuWoo Choi <kyuwoo.choi@gmail.com>
2019-11-25 20:30:47 +0900

    :sparkles: render inner slider (#2)
    * feat: make initial state
    
    * refactor: respect original design
    
    * feat: implement innerSlider.ssrInit
    
    * feat: implement settings onInit
    
    * feat: implement settings lazyLoad
    
    * fix: no unslick prop inherits
    
    * feat: implement class and dir attr
    
    * feat: implement track props
    
    * refactor: move common code
    
    * feat: implement list style & event
    
    * feat: implement setting dot prop & event
    
    * feat: implement setting arrow prop

---
+++ src/initialState.js
@@ -0,0 +1,23 @@
+export default {
+  animating: false,
+  autoplaying: null,
+  currentDirection: 0,
+  currentLeft: null,
+  currentSlide: 0,
+  direction: 1,
+  dragging: false,
+  edgeDragged: false,
+  initialized: false,
+  lazyLoadedList: [],
+  listHeight: null,
+  listWidth: null,
+  scrolling: false,
+  slideHeight: null,
+  slideWidth: null,
+  swipeLeft: null,
+  swiped: false, // used by swipeEvent. differentites between touch and swipe.
+  swiping: false,
+  touchObject: { startX: 0, startY: 0, curX: 0, curY: 0 },
+  trackStyle: {},
+  trackWidth: 0,
+}
