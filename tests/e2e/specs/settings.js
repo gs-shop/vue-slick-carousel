ad696ab59c01f4d6c5fdea6ab5046635d4093170
KyuWoo Choi <kyuwoo.choi@gmail.com>
2019-12-05 20:34:45 +0900

    :white_check_mark: test for settings adaptiveHeight

--- tests/e2e/specs/settings.js
+++ tests/e2e/specs/settings.js
@@ -15,6 +15,25 @@ describe('Settings', () => {
       cy.get('.slick-track .slick-slide:nth-child(1)').should('be.visible')
     })
   })
+  describe('adaptiveHeight', () => {
+    it('adapt slider height to match the height of the current slide', () => {
+      cy.visit('/example/adaptive-height')
+      let slideHeight
+      cy.get('.slick-active').then($slide => {
+        slideHeight = $slide[0].offsetHeight
+      })
+      cy.get('.slick-list').then($list => {
+        expect($list[0].offsetHeight).to.equal(slideHeight)
+      })
+      cy.get('.slick-slider').type('{rightarrow}')
+      cy.get('.slick-active').then($slide => {
+        slideHeight = $slide[0].offsetHeight
+      })
+      cy.get('.slick-list').then($list => {
+        expect($list[0].offsetHeight).to.equal(slideHeight)
+      })
+    })
+  })
   describe('rtl', () => {
     it('makes key navigation in reverse', () => {
       cy.visit('/example/rtl')
