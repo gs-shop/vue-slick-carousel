8d2360c438afe5b77415533daa3e6db0dc6cb6c6
KyuWoo Choi <kyuwoo.choi@gmail.com>
2019-12-28 15:53:15 +0900

    :white_check_mark: add test for api next

--- tests/e2e/specs/apis.js
+++ tests/e2e/specs/apis.js
@@ -6,4 +6,11 @@ describe('APIs', () => {
       cy.get('[data-index="8"]').should('be.visible')
     })
   })
+  describe('next', () => {
+    it('should move the slide left', () => {
+      cy.visit('/#/example/multiple')
+      cy.window().then(window => window.carousel.next())
+      cy.get('[data-index="3"]').should('be.visible')
+    })
+  })
 })
